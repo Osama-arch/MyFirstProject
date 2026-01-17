d1daba98469fd8830878bff13c459a245503a976
Osama-arch <Osamarchitectinfo@gmail.com>
2026-01-17 18:08:04 +0300

    initial commit

---
+++ FirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int firstNumber = 4;
+            Console.WriteLine($"Мне {firstNumber} года");
+        }
+    }
+}
